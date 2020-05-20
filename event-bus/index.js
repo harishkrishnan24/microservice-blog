d03e57281eeb5d06dc7643258b8043373ef4ae72
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-20 20:18:00 +0530

    Comment moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,11 @@ app.post("/events", (req, res) => {
 	axios.post("http://localhost:4001/events", event);
 	axios.post("http://localhost:4002/events", event);
 	axios.post("http://localhost:4003/events", event);
+	axios.post("http://localhost:4004/events", event);
 
 	res.send({ status: "OK" });
 });
 
 app.listen(4006, () => {
-	console.log("[EventBus] Listeneing on port 4006");
+	console.log("[EventBus] Listening on port 4006");
 });
